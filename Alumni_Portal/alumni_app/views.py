3401abdc11b9d46f45fbb37a923b36b912e48076
haxer-max <atharvashrawge@gmail.com>
2020-08-11 13:05:08 +0530

    login added

--- Alumni_Portal/alumni_app/views.py
+++ Alumni_Portal/alumni_app/views.py
@@ -1,18 +1,39 @@
 from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
+
 
 # Create your views here.
 # test test12345
 
 def home(request):
-    
+    if request.user.is_anonymous:
+        return redirect("/login")
     #return HttpResponse("hello")
-    return render(request,'alumni_app/index.html')
+    print(request.user)
+    return render(request,'alumni_app/home.html')
 
-def login(request):
+def loginUser(request):
+    if request.method =="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        print(password)
+        user=authenticate(username=username,password=password)    
+        print(user)
+        if user is not None:
+            login(request, user)
+            return redirect("/")     
+        else:
+            return render(request,'alumni_app/login.html')
+    
+    
     return render(request,'alumni_app/login.html')
 
-def logout(request):
-    return render(request,'alumni_app/logout.html')
+def logoutUser(request):
+    logout(request)
+    return redirect("/")
+    #return render(request,'alumni_app/logout.html')
 
 
 
